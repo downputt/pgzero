0c7b97a564cd00beeaed45ebcb86135e2ae7c500
downputt <downputtgd@gmail.com>
2026-06-11 18:09:11 +1000

    Add files via upload

---
+++ Landscape.py
@@ -0,0 +1,17 @@
+import pgzrun
+
+WIDTH = 300
+HEIGHT = 300
+
+def mountain():
+    point1 = (50,150)
+    point2 = (150,150)
+    point3 = (75,150)
+    screen.draw.filled_polygon([point1, point2, point3], (121, 114, 113))
+    point21 = (,150)
+    point22 = (150,150)
+    point23 = (75,150)
+    screen.draw.filled_polygon([point21, point22, point23], (121, 114, 113))
+
+
+pgzrun.go
